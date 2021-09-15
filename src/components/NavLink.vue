e7f94aa114e18d34b073c9762a90f30cc04fcde1
Ludovic Villain <chadludo@gmail.com>
2021-09-15 11:46:51 +0200

    Begin navbar

---
+++ src/components/NavLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <a class='navlink' href='url'>{{ text }}</a>
+</template>
+
+<script>
+export default ({
+  name: 'NavLink',
+  props: ['url', 'text']
+})
+</script>
+
+<style>
+  .navlink {
+    padding: 1rem 2rem;
+  }
+</style>
