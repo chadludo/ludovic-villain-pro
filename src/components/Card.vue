e13bc0bf99efa1ddcdf5521f9912ab98244edb96
ludovic villain <chadludo@gmail.com>
2021-12-16 15:25:48 +0100

    card overlay almost working

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
   <router-link v-for='work in works' :key='work.id' to='/'>
-    <div class='card flex flex-c'>
-      {{ work.image }}
-      <p>{{ work.date }}</p>
-      <h3>{{ work.category }}</h3>
-      <h2>{{ work.name }}</h2>
-      <p>{{ work.description }}</p>
+    <div class='card flex flex-c bgcolor' :style="{ backgroundImage : 'url(' + work.image_url + ')' }">
+      <div class='overlay'>
+        <p>{{ work.date }}</p>
+        <h3>{{ work.category }}</h3>
+        <h2>{{ work.name }}</h2>
+      </div>
     </div>
   </router-link>
 </template>
@@ -34,6 +34,7 @@ export default {
 @import '../assets/scss/_variables.scss';
 
   .card {
+    position: relative;
     width: 1fr;
     height: 400px;
     margin: 1rem 0;
@@ -42,14 +43,41 @@ export default {
     justify-content: center;
     align-items: center;
     background-size: cover;
+    border-radius: 0;
+    transition: .2s border-radius ease-in;
+
+    &:hover {
+      border-radius: 1.5rem;
+    }
+
+    p {
+      z-index: 2;
+    }
 
     h2 {
+      z-index: 2;
       font-size: $font-md;
     }
 
     h3 {
+      z-index: 2;
       font-size: $font-s;
       font-weight: $bold;
+    }
+
+    .overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      background-color: $primary;
+      transition: .2s opacity ease-in-out;
+
+      &:hover {
+        opacity: .8;
+      }
     }
   }
 </style>
